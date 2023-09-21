33ff11832ba424e0ff3d4471a8af52a4dfa0100f
aymane el yamani <aymaneelyamani3@gmail.com>
2023-09-21 23:19:44 +0100

    test one

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,13 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 int swipe;
 
+
+
+if (array == NULL || size < 2)
+{
+	return;
+}
+
 for (i = 0; i < size - 1; i++)
 {
 	for (j = 0; j < size - i - 1; j++)
